8e5a141642cc17b177b7903dc20b3bb8a918d3cc
David Andrews <dta@alumni.cmu.edu>
2019-06-05 18:23:03 -0400

    Fail silently if debug trap is overwritten

--- bashprompt.sh
+++ bashprompt.sh
@@ -31,6 +31,6 @@ PS1='$(
   fi
 )\$ '
 trap 'SECONDS_START=${SECONDS_START:-$SECONDS}' DEBUG
-PROMPT_COMMAND='if [[ "$?" != "0" ]] || [[ $(($SECONDS - $SECONDS_START)) > 3 ]]; then echo -n -e "\07"; fi; history -a; unset SECONDS_START'
+PROMPT_COMMAND='if [[ "$?" != "0" ]] || [[ $(($SECONDS - ${SECONDS_START:-$SECONDS})) > 3 ]]; then echo -n -e "\07"; fi; history -a; unset SECONDS_START'
 HISTTIMEFORMAT='%Y-%m-%d %T '
 
