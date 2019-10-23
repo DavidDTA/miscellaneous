9bd1a352a8d9173f38c5918749c4822120423d88
David Andrews <dta@alumni.cmu.edu>
2019-10-23 10:16:04 -0400

    Allow branch selection on push remote

--- push_remote.sh
+++ push_remote.sh
@@ -2,8 +2,9 @@ push_remote() {
   # Pushes the local working directory to dta-remote on github.  Does not change the working directory, index, or HEAD.
   local message="dta remote push at $(date -u +%FT%TZ)"
   local index=$(git write-tree)
+  local branch=${1:-dta-remote}
   git add --all
-  git update-ref refs/remotes/origin/dta-remote $(git commit-tree $(git write-tree) -p HEAD -m "$message")
+  git update-ref "refs/remotes/origin/$branch" $(git commit-tree $(git write-tree) -p HEAD -m "$message")
   git read-tree $index
-  git push origin refs/remotes/origin/dta-remote:refs/heads/dta-remote --force
+  git push origin "refs/remotes/origin/$branch:refs/heads/$branch" --force
 }
