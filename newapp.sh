fbb09168e6158824af9338ab97add739266394a4
David Andrews <dta@alumni.cmu.edu>
2020-07-01 11:05:24 -0400

    Add documentation

--- newapp.sh
+++ newapp.sh
@@ -1,4 +1,25 @@
-newapp() {                                                                                        
+newapp() {
+  # Usage: newapp app url profile
+  #
+  # Creates a barebones mac which wraps a website.
+  # The app will be indexed in spotlight for easy access.
+  #
+  # The script also opens vim to edit the script that will be
+  # run when the app is launched. This can be changed to have
+  # the app do something other than open a webpage as an app
+  # in Chrome, or it can be left alone by typing the three
+  # keys "colon", "q", and "enter".
+  #
+  # Arguments:
+  #   app:
+  #     The name of the app. The script will create a directory
+  #     with this name in the current directory, and the script
+  #     will be accessible in spotlight under this name.
+  #   url:
+  #     The url that serves as the homepage of the app.
+  #   profile:
+  #     The Chrome profile to use. Optional.
+  
   local appdir="${1//"'"/}"                                                                       
   local appdir="${appdir:-test}.app"                                                              
   local url="${2//"'"/}"
