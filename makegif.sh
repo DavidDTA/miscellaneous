9b1608ea572b2ce7f1f843d1c3bfd5abed3c0153
David Andrews <dta@alumni.cmu.edu>
2020-07-09 16:45:23 -0400

    Update makegif

--- makegif.sh
+++ makegif.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e;
 
+# Usage: makegif.sh in out
+
 FILTER='scale=300:-1';
 rm $2 || true;
 
 ffmpeg -i $1 -vf "$FILTER,palettegen" -y /tmp/palette.png;
-ffmpeg -i $f -i /tmp/palette.png -lavfi "$FILTER [x]; [x][1:v] paletteuse=dither=bayer" $2
+ffmpeg -i $1 -i /tmp/palette.png -lavfi "$FILTER [x]; [x][1:v] paletteuse=dither=bayer" $2
