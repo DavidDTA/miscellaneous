e018cbf3782c3ae5f9bc803c132c97e7f1692378
David Andrews <dta@alumni.cmu.edu>
2021-04-07 01:04:08 -0400

    Improve git prompt

--- color.sh
+++ color.sh
@@ -3,6 +3,8 @@ color() {
   # $2 is the text
   # $3 is the escape sequence start (optoinal)
   # $4 is the escape sequence end (optional)
+  #
+  # for color values, see https://robotmoon.com/256-colors/
   echo -ne "$3\x1b[$1$4"
   echo -ne "$2"
   echo -ne "$3\x1b[0m$4"
