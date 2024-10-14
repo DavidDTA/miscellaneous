54fd671cc106629366e449d6557000ff06111aab
David Oleander <dta@alumni.cmu.edu>
2024-10-14 02:52:20 +0000

    make watch more robust

--- flake/packages/watch/watch.py
+++ flake/packages/watch/watch.py
@@ -1,10 +1,11 @@
+import atexit
 import argparse
 import codecs
+import multiprocessing
 import os
 import psutil
-import queue
+import select
 import subprocess
-import threading
 
 
 _FSWATCH_BIN = (
@@ -13,66 +14,78 @@ _FSWATCH_BIN = (
 )
 
 
-def _process_output(out, callback):
-    for line in codecs.iterdecode(out, 'utf-8'):
-        callback(line)
+def _command_process(args, stdout_pipe_in, stderr_pipe_in, quit_pipe_out):
+    os.setsid()
+    popen = subprocess.Popen(
+        args,
+        stdin=subprocess.DEVNULL,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    open_input_streams = [quit_pipe_out, popen.stdout, popen.stderr]
+    while open_input_streams:
+        selected, _, _ = select.select(open_input_streams, [], [])
+        if popen.stdout in selected:
+            value = popen.stdout.read1()
+            if value:
+                stdout_pipe_in.send_bytes(value)
+            else:
+                open_input_streams.remove(popen.stdout)
+                stdout_pipe_in.close()
+        if popen.stderr in selected:
+            value = popen.stderr.read1()
+            if value:
+                stderr_pipe_in.send_bytes(value)
+            else:
+                open_input_streams.remove(popen.stderr)
+                stderr_pipe_in.close()
+        if quit_pipe_out in selected:
+            quit_pipe_out.recv()
+            open_input_streams.remove(quit_pipe_out)
+            quit_pipe_out.close()
+            _kill_session()
+    popen.wait()
+    while _kill_session():
+        pass
 
 
-def _enqueue_changed_file(q):
-    def callback(line):
-        q.put(line)
-    return callback
+def _kill_session():
+    """
+    returns:
+        True iff at least one process was terminated
+    """
+    pid = os.getpid()
+    sid = os.getsid(pid)
+    psutil.process_iter.cache_clear()
+    session_procs = [
+        candidate_proc
+        for candidate_proc in psutil.process_iter()
+        if os.getsid(candidate_proc.pid) == sid
+        and candidate_proc.pid != pid
+    ]
+    if not session_procs:
+        return False
+    for session_proc in session_procs:
+        session_proc.terminate()
+    psutil.wait_procs(session_procs)
+    return True
 
 
-def _enqueue_line_with_metadata(q, metadata):
-    def callback(line):
-        q.put((metadata, line))
-    return callback
+class Outputter:
+    def __init__(self):
+        self.last_source = None
+        self.incomplete_line = False
 
-
-def _merge_output(q):
-    last_name = None
-    while True:
-        ((name), line) = q.get()
-        if last_name != name:
-            if last_name is not None:
-                print(f'^^^ {last_name} ^^^')
-            print(f'vvv {name} vvv')
-            last_name = name
-        print(line.rstrip('\n'))
-
-
-def _start_output_thread(out, callback):
-    thread = threading.Thread(target=_process_output, args=(out, callback))
-    thread.daemon = True
-    thread.start()
-    return thread
-
-
-def _start_output_merge_thread(q):
-    thread = threading.Thread(target=_merge_output, args=(q,))
-    thread.daemon = True
-    thread.start()
-    return thread
-
-
-def _kill_session(proc):
-    proc.terminate()
-    sid = os.getsid(proc.pid)
-    while True:
-        psutil.process_iter.cache_clear()
-        session_procs = [
-            candidate_proc
-            for candidate_proc in psutil.process_iter()
-            if os.getsid(candidate_proc.pid) == sid
-            and candidate_proc.pid != proc.pid
-        ]
-        if not session_procs:
-            break
-        for session_proc in session_procs:
-            session_proc.terminate()
-        psutil.wait_procs(session_procs)
-    proc.wait()
+    def output(self, source, text):
+        if source != self.last_source:
+            if self.incomplete_line:
+                print()
+            if self.last_source is not None:
+                print(f'^^^ {self.last_source} ^^^')
+            print(f'vvv {source} vvv')
+        print(text, end='')
+        self.last_source = source
+        self.incomplete_line = not text.endswith("\n")
 
 
 def _main():
@@ -80,65 +93,115 @@ def _main():
     parser.add_argument('--paths', nargs='*', dest='paths', default=[])
     parser.add_argument('command', nargs='+')
     args = parser.parse_args()
-    fswatch_queue = queue.Queue()
-    output_queue = queue.Queue()
-    _start_output_merge_thread(output_queue)
-    if args.paths:
-        fswatch = subprocess.Popen(
-            [
-                _FSWATCH_BIN,
-                '--event',
-                'Created',
-                '--event',
-                'Updated',
-                '--event',
-                'Removed',
-                '--recursive',
-                *args.paths
-            ],
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL
-        )
-        _start_output_thread(
-            fswatch.stdout,
-            _enqueue_changed_file(fswatch_queue)
-        )
+    outputter = Outputter()
+    fswatch = subprocess.Popen(
+        [
+            _FSWATCH_BIN,
+            '--event',
+            'Created',
+            '--event',
+            'Updated',
+            '--event',
+            'Removed',
+            '--recursive',
+            *args.paths
+        ],
+        stdin=subprocess.DEVNULL,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    fswatch_stdout_decoder = codecs.getincrementaldecoder('utf-8')()
+    fswatch_stdout_buffer = ''
+    fswatch_stderr_decoder = codecs.getincrementaldecoder('utf-8')()
     while True:
-        command = None
-        try:
-            command = subprocess.Popen(
-                [*args.command],
-                stdin=subprocess.DEVNULL,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                start_new_session=True
+        stdout_pipe_out, stdout_pipe_in = multiprocessing.Pipe(False)
+        stderr_pipe_out, stderr_pipe_in = multiprocessing.Pipe(False)
+        quit_pipe_out, quit_pipe_in = multiprocessing.Pipe(False)
+        command_stdout_decoder = codecs.getincrementaldecoder('utf-8')()
+        command_stderr_decoder = codecs.getincrementaldecoder('utf-8')()
+
+        def cleanup():
+            quit_pipe_in.send(None)
+
+        def output_passthrough_streams(selected):
+            if fswatch.stderr in selected:
+                outputter.output(
+                    'error',
+                    fswatch_stderr_decoder.decode(fswatch.stderr.read1()),
+                )
+            if stdout_pipe_out in selected:
+                outputter.output(
+                    'command stdout',
+                    command_stdout_decoder.decode(
+                        stdout_pipe_out.recv_bytes(),
+                    ),
+                )
+            if stderr_pipe_out in selected:
+                outputter.output(
+                    'command stderr',
+                    command_stderr_decoder.decode(
+                        stderr_pipe_out.recv_bytes(),
+                    ),
+                )
+
+        atexit.register(cleanup)
+        command = multiprocessing.Process(
+            target=_command_process,
+            args=(args.command, stdout_pipe_in, stderr_pipe_in, quit_pipe_out),
+        )
+        command.start()
+        while True:
+            selected, _, _ = select.select(
+                [
+                    fswatch.stdout,
+                    fswatch.stderr,
+                    stdout_pipe_out,
+                    stderr_pipe_out,
+                ],
+                [],
+                [],
             )
-            command_thread_stdout = _start_output_thread(
-                command.stdout,
-                _enqueue_line_with_metadata(output_queue, 'command')
-            )
-            command_thread_stderr = _start_output_thread(
-                command.stderr,
-                _enqueue_line_with_metadata(output_queue, 'command')
-            )
-            modified = set()
-            modified.add(fswatch_queue.get())
-            output_queue.put(('watch', 'Stopping.'))
-            _kill_session(command)
-            command_thread_stdout.join()
-            command_thread_stderr.join()
-            while not fswatch_queue.empty():
-                modified.add(fswatch_queue.get())
-            message = "Modified:\n"
-            for file in sorted(modified):
-                message += "    " + file + "\n"
-            message += "Restarting."
-            output_queue.put(('watch', message))
-        except KeyboardInterrupt:
-            if command is not None:
-                _kill_session(command)
-            raise
+            output_passthrough_streams(selected)
+            if fswatch.stdout in selected:
+                outputter.output('watch', "Stopping.\n")
+                quit_pipe_in.send(None)
+                atexit.unregister(cleanup)
+                modified = set()
+                while True:
+                    selected, _, _ = select.select(
+                        [
+                            command.sentinel,
+                            fswatch.stdout,
+                            fswatch.stderr,
+                            stdout_pipe_out,
+                            stderr_pipe_out,
+                        ],
+                        [],
+                        [],
+                    )
+                    output_passthrough_streams(selected)
+                    if fswatch.stdout in selected:
+                        fswatch_stdout_buffer += fswatch_stdout_decoder.decode(
+                            fswatch.stdout.read1()
+                        )
+                        while True:
+                            split = fswatch_stdout_buffer.split("\n", 1)
+                            if len(split) > 1:
+                                modified.add(split[0])
+                                fswatch_stdout_buffer = split[1]
+                            else:
+                                fswatch_stdout_buffer = split[0]
+                                break
+                    if command.sentinel in selected:
+                        command.close()
+                        quit_pipe_in.close()
+                        message = "Modified:\n"
+                        for file in sorted(modified):
+                            message += "    " + file + "\n"
+                        message += "Restarting."
+                        outputter.output('watch', message)
+                        break
+                break
 
 
 if __name__ == '__main__':
