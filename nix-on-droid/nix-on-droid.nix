de54feaf90d84e944aa8f6ec2fabe95e0bcfe645
David Oleander <dta@alumni.cmu.edu>
2025-03-24 02:36:43 -0400

    Add nix on droid configuration

---
+++ nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    curl
+    git
+    gnugrep
+    jq
+    nix-search-cli
+    openssh
+    tmux
+    vim
+
+    # Some common stuff that people expect to have
+    #procps
+    #killall
+    #diffutils
+    #findutils
+    #utillinux
+    #tzdata
+    #hostname
+    #man
+    #gnugrep
+    #gnupg
+    #gnused
+    #gnutar
+    #bzip2
+    #gzip
+    #xz
+    #zip
+    #unzip
+  ];
+
+  # android-integration.termux-open.enable = true;
+  # android-integration.termux-open-url.enable = true;
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  # environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  time.timeZone = "America/New_York";
+
+}
