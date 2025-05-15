b25ac94ae30f32d4c03e81f35d1b01c8f9179c3d
David Oleander <dta@alumni.cmu.edu>
2025-05-15 19:00:07 -0400

    add unstable overlay

--- nix-on-droid/flake.nix
+++ nix-on-droid/flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/master";
@@ -10,12 +11,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, nix-on-droid }: {
-
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
-      modules = [ ./nix-on-droid.nix ];
+  outputs = { self, nixpkgs, nixpkgs-unstable, nix-on-droid }:
+    let
+      system = "aarch64-linux";
+      unstable-overlay = final: prev: with nixpkgs-unstable.legacyPackages.${system}; {
+      };
+    in
+    {
+      nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ unstable-overlay ];
+        };
+        modules = [ ./nix-on-droid.nix ];
+      };
     };
-
-  };
 }
