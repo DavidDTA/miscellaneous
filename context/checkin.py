8efbf8b3402b1d1573af4265c9b602a7a7954ca0
David Oleander <dta@alumni.cmu.edu>
2026-04-10 11:02:13 -0400

    Change log node based on staging

--- context/checkin.py
+++ context/checkin.py
@@ -203,4 +203,4 @@ if __name__ == '__main__':
   parser.add_argument('datafile')
   parser.add_argument('--unattest', action='store_true')
   args = parser.parse_args()
-  main(**vars(args))
+  #main(**vars(args))
