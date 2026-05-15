1ccddf30e39ad39e1ea25507b6ffaddb48c88da1
David Oleander <dta@alumni.cmu.edu>
2026-05-14 23:55:24 -0400

    Move addUnpackFallback to lib

--- nix/flake.nix
+++ nix/flake.nix
@@ -18,40 +18,50 @@
     let
       systems = pkgs.lib.systems.flakeExposed;
       overlay = final: prev: 
-        let
-          # The default unpack hook sometimes fails here:
-          # https://github.com/NixOS/nixpkgs/blob/6d7ec06d6868ac6d94c371458fc2391ded9ff13d/pkgs/stdenv/generic/setup.sh#L1256
-          # with:
-          # cp: setting permissions for 'source': No such file or directory
-          # It is unclear why, but this workaround produces the same end result
-          # We apply it surgically instead of in stdenv directly in order to avoid needing to rebuild everything
-          unpackFallback = prev.makeSetupHook { name = "unpack-fallback"; } (prev.writeText "unpack-fallback.sh" ''
-            _unpackFallback() {
-              local fn="$1"
-         
-              if [ ! -d "$fn" ]; then
-                return 1
-              fi
-         
-              local destination="$(stripHash "$fn")"
-              if [ -e "$destination" ]; then
-                echo "Cannot copy $fn to $destination: destination already exists!"
-                return 1
-              fi
-         
-              mkdir "$destination"
-              cp -r --preserve=timestamps --reflink=auto -- "$fn"/* "$destination"
-            }
-         
-            unpackCmdHooks+=(_unpackFallback)
-          '');
-          addUnpackFallback = pkg: pkg.overrideAttrs(old: {
-            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ unpackFallback ];
-          });
-        in
         {
         vimPlugins = prev.vimPlugins // {
-          vim-sensible = addUnpackFallback(prev.vimPlugins.vim-sensible);
+          vim-sensible = lib.addUnpackFallback(prev.vimPlugins.vim-sensible);
+        };
+      };
+      lib = {
+        addUnpackFallback =
+          let
+            # The default unpack hook sometimes fails here:
+            # https://github.com/NixOS/nixpkgs/blob/6d7ec06d6868ac6d94c371458fc2391ded9ff13d/pkgs/stdenv/generic/setup.sh#L1256
+            # with:
+            # cp: setting permissions for 'source': No such file or directory
+            # It is unclear why, but this workaround produces the same end result
+            # We apply it surgically instead of in stdenv directly in order to avoid needing to rebuild everything
+            unpackFallback = pkgs.makeSetupHook { name = "unpack-fallback"; } (pkgs.writeText "unpack-fallback.sh" ''
+              _unpackFallback() {
+                local fn="$1"
+           
+                if [ ! -d "$fn" ]; then
+                  return 1
+                fi
+           
+                local destination="$(stripHash "$fn")"
+                if [ -e "$destination" ]; then
+                  echo "Cannot copy $fn to $destination: destination already exists!"
+                  return 1
+                fi
+           
+                mkdir "$destination"
+                cp -r --preserve=timestamps --reflink=auto -- "$fn"/* "$destination"
+              }
+           
+              unpackCmdHooks+=(_unpackFallback)
+            '');
+          in
+          pkg: pkg.overrideAttrs(old: {
+            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ unpackFallback ];
+          });
+        mkNixOnDroidConfiguration = {}: nix-on-droid.lib.nixOnDroidConfiguration {
+          pkgs = pkgs;
+          modules = [ ./nix-on-droid.nix ];
+          extraSpecialArgs = {
+            localpkgs = packages.${pkgs.stdenv.hostPlatform.system};
+          };
         };
       };
       packages =
@@ -68,15 +78,7 @@
       };
     in
       {
+        lib = lib;
         packages = packages;
-        lib = {
-          mkNixOnDroidConfiguration = {}: nix-on-droid.lib.nixOnDroidConfiguration {
-            pkgs = pkgs;
-            modules = [ ./nix-on-droid.nix ];
-            extraSpecialArgs = {
-              localpkgs = packages.${pkgs.stdenv.hostPlatform.system};
-            };
-          };
-        };
       };
 }
